nayuta-ueno/lightning

commit 10057c8335228dd1187bafc554b8312945a947c4

openingd/json_fund_channel: - result fundchannel command now depends on successful or failed broadcast of the funding tx - failure returns error code FUNDING_BROADCAST_FAIL - don't fail the channel when broadcast failed, but keep in CHANNELD_AWAITING_LOCKIN - after fixing the initial broadcast failure, the user could manually rebroadcast the tx and   keep the channel
openingd/opening_funder_finished:
- broadcast_tx callback function now handles both success and failure

jsonrpc: added error code FUNDING_BROADCAST_FAIL
manpage: added error code returned by fundchannel command

This makes the user more aware of broadcast failure, so it hopefully doesn't
try to broadcast new tx's that depend on its change_outputs. Some users have reported (see
issue #2171) a whole sequence of fundings failing, because each funding was using the change
output of the previous one, which would not confirm.

lightningd/opening_control.c
@@ -66,15 +66,20 @@ struct uncommitted_channel {
 
 
 struct funding_channel {
-	struct command *cmd; /* Which also owns us. */
+	struct command *cmd; /* Which initially owns us until openingd request */
+
 	struct wallet_tx wtx;
 	u64 push_msat;
 	u8 channel_flags;
 
+	/* Variables we need to compose fields in cmd's response */
+	u8 *linear;
+	struct channel_id cid;
+
 	/* Peer we're trying to reach. */
 	struct pubkey peerid;
 
-	/* Channel. */
+	/* Channel, subsequent owner of us */
 	struct uncommitted_channel *uc;
 };
 
@@ -216,18 +221,56 @@ wallet_commit_channel(struct lightningd *ld,
 }
 
 static void funding_broadcast_failed(struct channel *channel,
-				     int exitstatus, const char *err)
+				     int exitstatus, const char *msg)
 {
-	channel_internal_error(channel,
-			       "Funding broadcast exited with %i: %s",
-			       exitstatus, err);
+	struct funding_channel *fc = channel->peer->uncommitted_channel->fc;
+	struct command *cmd = fc->cmd;
+
+	/* Massage output into shape so it doesn't kill the JSON serialization */
+	char *output = tal_strjoin(cmd, tal_strsplit(cmd, msg, "\n", STR_NO_EMPTY), " ", STR_NO_TRAIL);
+	was_pending(command_fail(cmd, FUNDING_BROADCAST_FAIL,
+			"Error broadcasting funding transaction: %s", output));
+
+	/* Frees fc too */
+	tal_free(fc->uc);
+
+	/* Keep in state CHANNELD_AWAITING_LOCKIN until (manual) broadcast */
+}
+
+static void funding_broadcast_success(struct channel *channel)
+{
+	struct json_stream *response;
+	struct funding_channel *fc = channel->peer->uncommitted_channel->fc;
+	struct command *cmd = fc->cmd;
+
+	response = json_stream_success(cmd);
+	json_object_start(response, NULL);
+	json_add_hex_talarr(response, "tx", fc->linear);
+	json_add_txid(response, "txid", &channel->funding_txid);
+	json_add_string(response, "channel_id",
+					type_to_string(tmpctx, struct channel_id, &fc->cid));
+	json_object_end(response);
+	was_pending(command_success(cmd, response));
+
+	/* Frees fc too */
+	tal_free(fc->uc);
+}
+
+static void funding_broadcast_failed_or_success(struct channel *channel,
+				     int exitstatus, const char *msg)
+{
+	if (exitstatus == 0) {
+		funding_broadcast_success(channel);
+	} else {
+		funding_broadcast_failed(channel, exitstatus, msg);
+	}
 }
 
 static void opening_funder_finished(struct subd *openingd, const u8 *resp,
 				    const int *fds,
 				    struct funding_channel *fc)
 {
-	u8 *msg, *linear;
+	u8 *msg;
 	struct channel_info channel_info;
 	struct bitcoin_tx *fundingtx;
 	struct bitcoin_txid funding_txid, expected_txid;
@@ -239,9 +282,7 @@ static void opening_funder_finished(struct subd *openingd, const u8 *resp,
 	u32 feerate;
 	u64 change_satoshi;
 	struct channel *channel;
-	struct json_stream *response;
 	struct lightningd *ld = openingd->ld;
-	struct channel_id cid;
 
 	assert(tal_count(fds) == 2);
 
@@ -372,32 +413,23 @@ static void opening_funder_finished(struct subd *openingd, const u8 *resp,
 	if (tal_count(fundingtx->output) == 2)
 		txfilter_add_scriptpubkey(ld->owned_txfilter, fundingtx->output[!funding_outnum].script);
 
-	/* Send it out and watch for confirms. */
-	broadcast_tx(ld->topology, channel, fundingtx, funding_broadcast_failed);
+	/* We need these to compose cmd's response in funding_broadcast_success */
+	fc->linear = linearize_tx(fc->cmd, fundingtx);
+	derive_channel_id(&fc->cid, &channel->funding_txid, funding_outnum);
 
+	/* Send it out and watch for confirms. */
+	broadcast_tx(ld->topology, channel, fundingtx, funding_broadcast_failed_or_success);
 	channel_watch_funding(ld, channel);
+
+	/* Mark consumed outputs as spent */
+	wallet_confirm_utxos(ld->wallet, fc->wtx.utxos);
 
 	/* Start normal channel daemon. */
 	peer_start_channeld(channel, &cs, fds[0], fds[1], NULL, false);
 
-	wallet_confirm_utxos(ld->wallet, fc->wtx.utxos);
-
-	response = json_stream_success(fc->cmd);
-	json_object_start(response, NULL);
-	linear = linearize_tx(response, fundingtx);
-	json_add_hex_talarr(response, "tx", linear);
-	json_add_txid(response, "txid", &channel->funding_txid);
-	derive_channel_id(&cid, &channel->funding_txid, funding_outnum);
-	json_add_string(response, "channel_id",
-			type_to_string(tmpctx, struct channel_id, &cid));
-	json_object_end(response);
-	was_pending(command_success(fc->cmd, response));
-
 	subd_release_channel(openingd, fc->uc);
 	fc->uc->openingd = NULL;
 
-	/* Frees fc too, and tmpctx */
-	tal_free(fc->uc);
 	return;
 
 failed:

common/jsonrpc_errors.h
@@ -39,6 +39,7 @@
 #define FUND_MAX_EXCEEDED               300
 #define FUND_CANNOT_AFFORD              301
 #define FUND_OUTPUT_IS_DUST             302
+#define FUNDING_BROADCAST_FAIL          303
 
 /* Errors from `invoice` command */
 #define INVOICE_LABEL_ALREADY_EXISTS	900

doc/lightning-fundchannel.7
@@ -2,12 +2,12 @@
 .\"     Title: lightning-fundchannel
 .\"    Author: [FIXME: author] [see http://docbook.sf.net/el/author]
 .\" Generator: DocBook XSL Stylesheets v1.79.1 <http://docbook.sf.net/>
-.\"      Date: 12/07/2018
+.\"      Date: 01/22/2019
 .\"    Manual: \ \&
 .\"    Source: \ \&
 .\"  Language: English
 .\"
-.TH "LIGHTNING\-FUNDCHANN" "7" "12/07/2018" "\ \&" "\ \&"
+.TH "LIGHTNING\-FUNDCHANN" "7" "01/22/2019" "\ \&" "\ \&"
 .\" -----------------------------------------------------------------
 .\" * Define some portability stuff
 .\" -----------------------------------------------------------------
@@ -93,6 +93,17 @@ The following error codes may occur:
 .IP \(bu 2.3
 .\}
 302\&. The output amount is too small, and would be considered dust\&.
+.RE
+.sp
+.RS 4
+.ie n \{\
+\h'-04'\(bu\h'+03'\c
+.\}
+.el \{\
+.sp -1
+.IP \(bu 2.3
+.\}
+303\&. Broadcasting of the funding transaction failed, the internal call to bitcoin\-cli returned with an error\&.
 .RE
 .sp
 Failure may also occur if \fBlightningd\fR and the peer cannot agree on channel parameters (funding limits, channel reserves, fees, etc\&.)\&.

lightningd/chaintopology.c
@@ -200,8 +200,8 @@ static void broadcast_done(struct bitcoind *bitcoind,
 	/* No longer needs to be disconnected if channel dies. */
 	tal_del_destructor2(otx->channel, clear_otx_channel, otx);
 
-	if (otx->failed && exitstatus != 0) {
-		otx->failed(otx->channel, exitstatus, msg);
+	if (otx->failed_or_success) {
+		otx->failed_or_success(otx->channel, exitstatus, msg);
 		tal_free(otx);
 	} else {
 		/* For continual rebroadcasting, until channel freed. */
@@ -213,7 +213,7 @@ static void broadcast_done(struct bitcoind *bitcoind,
 
 void broadcast_tx(struct chain_topology *topo,
 		  struct channel *channel, const struct bitcoin_tx *tx,
-		  void (*failed)(struct channel *channel,
+		  void (*failed_or_success)(struct channel *channel,
 				 int exitstatus, const char *err))
 {
 	/* Channel might vanish: topo owns it to start with. */
@@ -223,7 +223,7 @@ void broadcast_tx(struct chain_topology *topo,
 	otx->channel = channel;
 	bitcoin_txid(tx, &otx->txid);
 	otx->hextx = tal_hex(otx, rawtx);
-	otx->failed = failed;
+	otx->failed_or_success = failed_or_success;
 	tal_free(rawtx);
 	tal_add_destructor2(channel, clear_otx_channel, otx);
 

lightningd/chaintopology.h
@@ -37,7 +37,7 @@ struct outgoing_tx {
 	struct channel *channel;
 	const char *hextx;
 	struct bitcoin_txid txid;
-	void (*failed)(struct channel *channel, int exitstatus, const char *err);
+	void (*failed_or_success)(struct channel *channel, int exitstatus, const char *err);
 };
 
 struct block {

tests/test_connection.py
@@ -1120,7 +1120,8 @@ def test_fundee_forget_funding_tx_unconfirmed(node_factory, bitcoind):
     # Fund the channel.
     # The process will complete, but funder will be unable
     # to broadcast and confirm funding tx.
-    l1.rpc.fundchannel(l2.info['id'], 10**6)
+    with pytest.raises(RpcError, match=r'sendrawtransaction disabled'):
+        l1.rpc.fundchannel(l2.info['id'], 10**6)
 
     # Generate blocks until unconfirmed.
     bitcoind.generate_block(blocks)

commit b648e53c1313451f947e1b16e0be876d941532ea

opening: Delay adding change outputs when funding fails
This was proposed in #2274, as a way to avoid chaining multiple
unconfirmed spends, by delaying the addition of change when we fail
the broadcast. The main difference is when we extract our change
output from a funding transaction and when we mark the reserved coins
a spent.

In case of failure, we'll still add the change script to the txfilter,
but wait for confirmation before adding the change output
itself. We'll also not mark the outputs as spent, but rather keep them
reserved.

Signed-off-by: Christian Decker <decker.christian@gmail.com>

lightningd/opening_control.c
@@ -81,6 +81,7 @@ struct funding_channel {
 
 	/* Channel, subsequent owner of us */
 	struct uncommitted_channel *uc;
+	struct bitcoin_tx *fundingtx;
 };
 
 static void uncommitted_channel_disconnect(struct uncommitted_channel *uc,
@@ -242,6 +243,14 @@ static void funding_broadcast_success(struct channel *channel)
 	struct json_stream *response;
 	struct funding_channel *fc = channel->peer->uncommitted_channel->fc;
 	struct command *cmd = fc->cmd;
+	u64 change_satoshi;
+
+	/* Extract the change output and add it to the DB */
+	wallet_extract_owned_outputs(cmd->ld->wallet, fc->fundingtx, NULL,
+				     &change_satoshi);
+
+	/* Mark consumed outputs as spent */
+	wallet_confirm_utxos(cmd->ld->wallet, fc->wtx.utxos);
 
 	response = json_stream_success(cmd);
 	json_object_start(response, NULL);
@@ -272,7 +281,6 @@ static void opening_funder_finished(struct subd *openingd, const u8 *resp,
 {
 	u8 *msg;
 	struct channel_info channel_info;
-	struct bitcoin_tx *fundingtx;
 	struct bitcoin_txid funding_txid, expected_txid;
 	struct pubkey changekey;
 	struct crypto_state cs;
@@ -280,7 +288,6 @@ static void opening_funder_finished(struct subd *openingd, const u8 *resp,
 	struct bitcoin_tx *remote_commit;
 	u16 funding_outnum;
 	u32 feerate;
-	u64 change_satoshi;
 	struct channel *channel;
 	struct lightningd *ld = openingd->ld;
 
@@ -322,7 +329,7 @@ static void opening_funder_finished(struct subd *openingd, const u8 *resp,
 			     &changekey, fc->wtx.change_key_index))
 		fatal("Error deriving change key %u", fc->wtx.change_key_index);
 
-	fundingtx = funding_tx(tmpctx, &funding_outnum,
+	fc->fundingtx = funding_tx(fc, &funding_outnum,
 			       fc->wtx.utxos, fc->wtx.amount,
 			       &fc->uc->local_funding_pubkey,
 			       &channel_info.remote_fundingkey,
@@ -330,18 +337,18 @@ static void opening_funder_finished(struct subd *openingd, const u8 *resp,
 			       ld->wallet->bip32_base);
 
 	log_debug(fc->uc->log, "Funding tx has %zi inputs, %zu outputs:",
-		  tal_count(fundingtx->input),
-		  tal_count(fundingtx->output));
+		  tal_count(fc->fundingtx->input),
+		  tal_count(fc->fundingtx->output));
 
-	for (size_t i = 0; i < tal_count(fundingtx->input); i++) {
+	for (size_t i = 0; i < tal_count(fc->fundingtx->input); i++) {
 		log_debug(fc->uc->log, "%zi: %"PRIu64" satoshi (%s) %s\n",
 			  i, fc->wtx.utxos[i]->amount,
 			  fc->wtx.utxos[i]->is_p2sh ? "P2SH" : "SEGWIT",
 			  type_to_string(tmpctx, struct bitcoin_txid,
-					 &fundingtx->input[i].txid));
+					 &fc->fundingtx->input[i].txid));
 	}
 
-	bitcoin_txid(fundingtx, &funding_txid);
+	bitcoin_txid(fc->fundingtx, &funding_txid);
 
 	if (!bitcoin_txid_eq(&funding_txid, &expected_txid)) {
 		log_broken(fc->uc->log,
@@ -400,29 +407,23 @@ static void opening_funder_finished(struct subd *openingd, const u8 *resp,
 		fatal("Could not write to HSM: %s", strerror(errno));
 
 	msg = wire_sync_read(fc, ld->hsm_fd);
-	if (!fromwire_hsm_sign_funding_reply(tmpctx, msg, &fundingtx))
+	if (!fromwire_hsm_sign_funding_reply(fc, msg, &fc->fundingtx))
 		fatal("HSM gave bad sign_funding_reply %s",
 		      tal_hex(msg, resp));
-
-	/* Extract the change output and add it to the DB */
-	wallet_extract_owned_outputs(ld->wallet, fundingtx, NULL, &change_satoshi);
 
 	/* Make sure we recognize our change output by its scriptpubkey in
 	 * future. This assumes that we have only two outputs, may not be true
 	 * if we add support for multifundchannel */
-	if (tal_count(fundingtx->output) == 2)
-		txfilter_add_scriptpubkey(ld->owned_txfilter, fundingtx->output[!funding_outnum].script);
+	if (tal_count(fc->fundingtx->output) == 2)
+		txfilter_add_scriptpubkey(ld->owned_txfilter, fc->fundingtx->output[!funding_outnum].script);
 
 	/* We need these to compose cmd's response in funding_broadcast_success */
-	fc->linear = linearize_tx(fc->cmd, fundingtx);
+	fc->linear = linearize_tx(fc->cmd, fc->fundingtx);
 	derive_channel_id(&fc->cid, &channel->funding_txid, funding_outnum);
 
 	/* Send it out and watch for confirms. */
-	broadcast_tx(ld->topology, channel, fundingtx, funding_broadcast_failed_or_success);
+	broadcast_tx(ld->topology, channel, fc->fundingtx, funding_broadcast_failed_or_success);
 	channel_watch_funding(ld, channel);
-
-	/* Mark consumed outputs as spent */
-	wallet_confirm_utxos(ld->wallet, fc->wtx.utxos);
 
 	/* Start normal channel daemon. */
 	peer_start_channeld(channel, &cs, fds[0], fds[1], NULL, false);